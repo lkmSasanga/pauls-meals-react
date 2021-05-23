7a3879dff408e576c9468c8181c54e8917f712a8
lkmSasanga <lkmsasanga@gmail.com>
2021-05-23 16:56:45 +0530

    meals components added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Header from "./components/Layout/Header";
 import {Fragment} from "react";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <Fragment>
-        {/*<h1>Helloooo...</h1>*/}
       <Header/>
+        <main>
+            <Meals/>
+        </main>
     </Fragment>
   );
 }
